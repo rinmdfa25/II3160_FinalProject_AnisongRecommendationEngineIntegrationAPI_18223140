dc68bc35a4cd3c508a29b19b4a7ffd0c108fd485
Raihan M. Daffa <163854172+rinmdfa25@users.noreply.github.com>
2025-11-17 13:44:47 +0700

    feat: add anisongs searching based on criteria like season, year, genre

--- src/routers/anisong.py
+++ src/routers/anisong.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Query
-from src.services.anisong_services import fetch_anisong_list, fetch_anisong_name
+from src.services.anisong_services import fetch_anisong_list, fetch_anisong_name, fetch_anisong_criteria
 from src.services.youtube_service import search_youtube
 from src.services.spotify_services import search_spotify
+from typing import Optional
 import asyncio
 
 
@@ -11,7 +12,7 @@ router = APIRouter(
 )
 
 @router.get("/themes")
-async def search_anisongs_by_theme(theme_type: str = Query(..., regex="^(OP|ED|INS)$"), limit: int = Query(5, ge=1, le=100)):
+async def search_anisongs_by_theme(theme_type: str = Query(..., regex="^(OP|ED|INS)$"), limit: int = Query(5, ge=1, le=50)):
     anisongs = await fetch_anisong_list(theme_type, limit)
     anisongs_results = []
     
@@ -80,7 +81,6 @@ async def search_anisong_by_name(
             elif provider == "both":
                 raw_yt_url, raw_sp_url = response
 
-            
             if isinstance(raw_yt_url, BaseException):
                 yt_url = None
             else:
@@ -100,3 +100,53 @@ async def search_anisong_by_name(
         })
 
     return {"count": len(anisongs_only), "results": anisongs_only}
+
+@router.get("/criteria")
+async def search_anisong_by_criteria(
+    year: Optional[int] = Query(None, description="Filter by year (2025|2024|2023|2022|...)"),
+    season: Optional[str] = Query(None, regex="^(Winter|Spring|Summer|Fall)"),
+    genre: Optional[str] = Query(None, description="Filter by genre (Action|Romance|Comedy|Drama|...)"),
+    limit: int = Query(25, ge=1, le=50)
+):
+    
+    anisongs = await fetch_anisong_criteria(
+        year=year,
+        season=season,
+        genre=genre,
+        limit=limit
+    )
+    
+    if not anisongs:
+        return {"count": 0, "results": []}
+    
+    anisongs_results = []
+    for song in anisongs:
+        title = song.get("song_title", "")
+        artists = song.get("artists", [])
+        
+        main_artist = artists[0] if artists else ""
+        query = f"{title} {main_artist} {song.get('anime', '')}"
+        
+        yt_task = search_youtube(query)
+        sp_task = search_spotify(title, main_artist)
+        raw_yt_url, raw_sp_url = await asyncio.gather(yt_task, sp_task, return_exceptions=True)
+
+        if isinstance(raw_yt_url, BaseException):
+            yt_url = None
+        else:
+            yt_url = raw_yt_url
+        
+        if isinstance(raw_sp_url, BaseException):
+            sp_url = None
+        else:
+            sp_url = raw_sp_url
+            
+        anisongs_results.append({
+            "anime": song["anime"],
+            "song_title": song["song_title"],
+            "theme_type": song["theme_type"],
+            "youtube_url": yt_url,
+            "spotify_url": sp_url
+        })    
+        
+    return {"count": len(anisongs_results), "results": anisongs_results}
--- src/services/anisong_services.py
+++ src/services/anisong_services.py
@@ -1,13 +1,15 @@
 import httpx
+from fastapi import Query
+from typing import Optional 
 
 BASE_URL = "https://api.animethemes.moe"
 
-async def fetch_anisong_list(theme_type: str = "OP", limit: int = 5):
+async def fetch_anisong_list(theme_type: str, limit: int = 25):
     url = (
         f"{BASE_URL}/animetheme?"
         f"filter[type]={theme_type}"
         f"&sort=-anime.year"
-        f"&limit={limit}"
+        f"&page[size]={limit}"
         f"&include=anime,animethemeentries,song.artists"
     )
     
@@ -59,4 +61,54 @@ async def fetch_anisong_name(name: str, limit: int = 5):
             })
     
     return anime_names     
-            
+        
+async def fetch_anisong_criteria(
+    year: Optional[int] = None,
+    season: Optional[str] = None,
+    genre: Optional[str] = None,
+    limit: int = 25
+):
+    base_url = f"{BASE_URL}/anime"
+    
+    params = {
+        "include": "animethemes.song.artists,animethemes.animethemeentries.videos",
+        "page[size]": limit
+    }
+    
+    criteria_filters = {}
+    if year:
+        criteria_filters["year"] = year
+    if season:
+        criteria_filters["season"] = season
+    if genre:
+        criteria_filters["genre"] = genre
+        
+    criteria_filter_str = "&".join(criteria_filters)
+    
+    if criteria_filters:
+        params["filter"] = criteria_filters
+        
+    url = (
+        f"{base_url}"
+        f"?{criteria_filter_str}"
+        f"&include=animethemes.song.artists,animethemes.animethemeentries.videos"
+        f"&page[size]={limit}"
+    )    
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        response.raise_for_status()
+        data = response.json()
+        
+    anime_songs = []
+    for anime in data.get("anime", []):
+        anime_name = anime.get("name")
+        for theme in anime.get("animethemes", []):
+            song = theme.get("song", {})
+            anime_songs.append({
+                "anime": anime_name,
+                "song_title": song.get("title"),
+                "artists" : [a.get("name") for a in song.get("artists", [])],
+                "theme_type": theme.get("type")
+            })  
+    return anime_songs 
