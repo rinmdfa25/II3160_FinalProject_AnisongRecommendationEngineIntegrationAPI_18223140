6c3d028b21cb082bacc176fffb48266ecd7eef87
Raihan M. Daffa <163854172+rinmdfa25@users.noreply.github.com>
2025-11-21 16:02:15 +0700

    fix: updated secret key code with payload standarization within hex-32

--- src/services/auth_services.py
+++ src/services/auth_services.py
@@ -4,7 +4,7 @@ import os
 import jwt
 import time
 
-SECRET_KEY = os.getenv("JWT_SECRET", "dev_secret_key",)
+SECRET_KEY = os.getenv("JWT_SECRET")
 ALGO = "HS256"
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -16,13 +16,18 @@ def verify_password(plain, hashed):
     return pwd_context.verify(plain, hashed)
 
 def create_token(user_id):
+    now = int(time.time())
     payload = {
         "sub": str(user_id),
-        "exp": int(time.time()) + 3600
-        }
-    
+        "iat": now,
+        "exp": now + 3600,
+        "type": "access"
+    }
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGO)
 
-def decode_token(token):
-    return jwt.decode(token, SECRET_KEY, algorithms=[ALGO])
+def decode_token(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGO])
+    if payload.get("type") != "access":
+        raise ValueError("Invalid token type")
+    return payload
 
